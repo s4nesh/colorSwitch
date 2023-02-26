e610943f798974a4c0cdfd3dd1d5d8480d8591bb
Sanesh Gobin <saneshgobin@icloud.com>
2023-02-26 18:02:29 +0200

    Create variables for each switch and reference as outlets Create Actions for each switch and implement logic code for each switch

--- colorSwitch/ViewController.swift
+++ colorSwitch/ViewController.swift
@@ -9,11 +9,69 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var colorBox: UIView!
+    @IBOutlet var redSwitch: UISwitch!
+    @IBOutlet var greenSwitch: UISwitch!
+    @IBOutlet var blueSwitch: UISwitch!
+    
+    var toggleRedSwitch = false
+    var toggleGreenSwitch = false
+    var toggleBlueSwitch = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        switchAllOff()
     }
 
-
+    func switchAllOff() {
+        redSwitch.setOn(false, animated: true)
+        greenSwitch.setOn(false, animated: true)
+        blueSwitch.setOn(false, animated: true)
+    }
+    
+    @IBAction func redSwitchToggle(_ sender: Any) {
+        toggleRedSwitch.toggle()
+        if redSwitch.isOn {
+            switchAllOff()
+            redSwitch.setOn(true, animated: true)
+            colorBox.backgroundColor = .red
+            redSwitch.onTintColor = .red
+        } else {
+            redSwitch.setOn(false, animated: true)
+            colorBox.backgroundColor = .black
+        }
+        
+    }
+    
+    
+    @IBAction func greenSwitchToggle(_ sender: Any) {
+        toggleGreenSwitch.toggle()
+        if greenSwitch.isOn {
+            switchAllOff()
+            greenSwitch.setOn(true, animated: true)
+            colorBox.backgroundColor = .green
+            greenSwitch.onTintColor = .green
+        } else {
+            greenSwitch.setOn(false, animated: true)
+            colorBox.backgroundColor = .black
+        }
+    }
+    
+    
+    @IBAction func blueSwitchToggle(_ sender: Any) {
+        toggleBlueSwitch.toggle()
+        if blueSwitch.isOn {
+            switchAllOff()
+            blueSwitch.setOn(true, animated: true)
+            colorBox.backgroundColor = .blue
+            blueSwitch.onTintColor = .blue
+        } else {
+            blueSwitch.setOn(false, animated: true)
+            colorBox.backgroundColor = .black
+        }
+    }
+    
+    
 }
 
